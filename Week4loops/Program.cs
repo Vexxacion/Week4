85904643d7a32136e806624627376be25ee32783
TePee <teven.palmsaar@tktk.ee>
2024-04-05 15:15:50 +0300

    add week 4

---
+++ Week4loops/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// FOR-LOOP
+// Dry-Principle - Do not repeat yourself.
+
+
+
+using System.Runtime.InteropServices.Marshalling;
+int i = 0;
+
+for(i = 0; i < 5; i++) //i++ = i + 1
+{
+    Console.WriteLine("We dont need no education.");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day!");
+
+
+//Console.WriteLine( "We dont need no education");
